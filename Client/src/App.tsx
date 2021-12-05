44d4ab06fcd29bd7a3c30251cc397adaf7e0b31c
Pondigo <carlos.pondigosa@udlap.mx>
2021-12-05 13:17:03 -0600

    Start the project

---
+++ Client/src/App.tsx
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import './App.css';
+import React, { useState } from 'react'
+
+function App() {
+
+  var [textToSearch, setTextToSearch] = useState('')
+  // onClick of startSession_button
+  const loginFunction = (e: any) => {
+
+    axios.get("http://localhost:3001/getAuthURL").then(res => {
+      console.log(res.data.url)
+      window.open(res.data.url, '_blank', 'noopener,noreferrer')
+    })
+  }
+  const startMap = (e: any) => {
+    axios.post(`http://localhost:3001/mapFilesOnedrive`, { address: textToSearch })
+      .then(res => {
+        console.log(res);
+        //console.log(res.data);
+      })
+  }
+
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button className="startSession_button" onClick={loginFunction}>Inicia session</button>
+        <input type="text" id='inputText' className='searchInput' placeholder='Path where download' onChange={(e) => setTextToSearch(e.target.value)} />
+        <button onClick={startMap}>Map</button>
+        <button>Download</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
